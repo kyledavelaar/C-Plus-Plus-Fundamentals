5b9099e4955680d10e2ef720b0b31d8440738518
Kyle Davelaar <kyle.x.davelaar.-ND@disney.com>
2022-04-27 06:39:36 -0700

    all

--- forward_delcaration.cpp
+++ forward_delcaration.cpp
@@ -5,6 +5,15 @@ int add(int x, int y);
 
 int main() {
     std::cout << "The sum is " << add(2,3) << '\n';
+
+    // can also make a declaration via 'using'
+    using std::cout;
+    cout << "I did not have to write std:: here " << '\n';
+
+    // or can make a directive (but this is dangerous so don't do it, naming collisions)
+    using namespace std;
+    cout << "another way";
+
     return 0;
 }
 
--- namespace.cpp
+++ namespace.cpp
@@ -34,6 +34,32 @@ namespace roo
 //     }
 // }
 
+namespace // unnamed namespace
+{
+    void doSomething() // can only be accessed in this file, same functionality as if you made this a 'static' method
+    {
+        std::cout << "like static, only this file can call this\n";
+    }
+    // Unnamed namespaces will also keep user-defined types local to the file,
+}
+
+// inline namespaces are considered part of the parent namespace (often used for versioning)
+inline namespace v1 // declare an inline namespace named v1
+{
+    void doSomethingCool()
+    {
+        std::cout << "v1\n";
+    }
+}
+
+namespace v2 // declare a normal namespace named v2
+{
+    void doSomethingCool()
+    {
+        std::cout << "v2\n";
+    }
+}
+
 int main() {
     foo::say();
     goo::say();
@@ -41,5 +67,11 @@ int main() {
     // namespace alias
     namespace shorter = roo::nestedRoo;
     shorter::say();
+
+    doSomething(); // we can call doSomething() without a namespace prefix
+
+    v1::doSomethingCool(); // calls the v1 version of doSomething()
+    v2::doSomethingCool(); // calls the v2 version of doSomething()
+    doSomethingCool(); // calls the inline version of doSomething() (which is v1)
     return 0;
 }
