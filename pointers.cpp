cf91ce19af7bf56818131102dc1aad0eae612a0d
Kyle Davelaar <kyle.x.davelaar.-ND@disney.com>
2022-05-06 19:51:15 -0700

    structs

--- pointers.cpp
+++ pointers.cpp
@@ -4,7 +4,7 @@ int main() {
 
     int x{ 5 };
     std::cout << x << '\n';  // print the value of variable x
-    std::cout << &x << '\n'; // print the memory address of variable x
+    std::cout << &x << '\n'; // print the memory address of variable x (& aka address of operator)
     // * here is a DEFERENCE OPERATOR (not a pointer)
     std::cout << *(&x) << '\n'; // print the value at the memory address of variable x (parentheses not required, but make it easier to read)
 
